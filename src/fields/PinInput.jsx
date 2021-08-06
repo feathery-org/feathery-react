66d36653faf5ceede553ebb57f950878fa414bc1
bo-dun-1 <pdun2008@gmail.com>
2021-08-06 13:29:08 -0500

    migrate pin input

--- src/fields/PinInput.jsx
+++ src/fields/PinInput.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PinInput, PinInputField } from '@chakra-ui/pin-input';
+import OtpInput from 'react-otp-input';
 import { reactFriendlyKey } from '../utils/formHelperFunctions';
 import { borderStyleFromField, marginStyleFromField } from '../utils/styles';
 
@@ -26,43 +26,36 @@ function FeatheryPinInput({
             }}
         >
             {fieldLabel}
-            <PinInput
+            <OtpInput
                 id={servar.key}
-                defaultValue={fieldVal}
+                value={fieldVal}
                 onChange={onChange}
                 onClick={onClick}
-            >
-                {Array(servar.max_length)
-                    .fill(0)
-                    .map((_, i) => (
-                        <PinInputField
-                            key={`pin-${i}`}
-                            style={{
-                                textAlign: 'center',
-                                marginLeft: '8px',
-                                outline: 'none',
-                                height: `${field.styles.field_height}${field.styles.field_height_unit}`,
-                                width: `${field.styles.field_width}${field.styles.field_width_unit}`,
-                                backgroundColor: `#${field.styles.background_color}`,
-                                boxShadow: `${field.styles.shadow_x_offset}px ${field.styles.shadow_y_offset}px ${field.styles.shadow_blur_radius}px #${field.styles.shadow_color}`,
-                                fontSize: `${field.styles.font_size}px`,
-                                color: `#${field.styles.font_color}`,
-                                borderRadius: field.borderRadius,
-                                ...borderStyle
-                            }}
-                            css={{
-                                '&::placeholder': {
-                                    color: `#${field.styles.font_color} !important`
-                                },
-                                '&:focus': {
-                                    boxShadow: `${field.styles.shadow_x_offset}px ${field.styles.shadow_y_offset}px ${field.styles.shadow_blur_radius}px #${field.styles.shadow_color} !important`,
-                                    ...selectCSS
-                                },
-                                '&:hover': hoverCSS
-                            }}
-                        />
-                    ))}
-            </PinInput>
+                numInputs={servar.max_length}
+                inputStyle={{
+                    textAlign: 'center',
+                    marginLeft: '8px',
+                    outline: 'none',
+                    height: `${field.styles.field_height}${field.styles.field_height_unit}`,
+                    width: `${field.styles.field_width}${field.styles.field_width_unit}`,
+                    backgroundColor: `#${field.styles.background_color}`,
+                    boxShadow: `${field.styles.shadow_x_offset}px ${field.styles.shadow_y_offset}px ${field.styles.shadow_blur_radius}px #${field.styles.shadow_color}`,
+                    fontSize: `${field.styles.font_size}px`,
+                    color: `#${field.styles.font_color}`,
+                    borderRadius: field.borderRadius,
+                    ...borderStyle
+                }}
+                css={{
+                    '&::placeholder': {
+                        color: `#${field.styles.font_color} !important`
+                    },
+                    '&:focus': {
+                        boxShadow: `${field.styles.shadow_x_offset}px ${field.styles.shadow_y_offset}px ${field.styles.shadow_blur_radius}px #${field.styles.shadow_color} !important`,
+                        ...selectCSS
+                    },
+                    '&:hover': hoverCSS
+                }}
+            />
         </div>
     );
 }
