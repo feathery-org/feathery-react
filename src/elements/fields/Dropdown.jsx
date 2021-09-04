f5389edc6dfd66a05204cc3f1b5ab1fd3ada9056
bo-dun-1 <pdun2008@gmail.com>
2021-09-04 13:01:19 -0400

    dropdown placeholder support

--- src/elements/fields/Dropdown.jsx
+++ src/elements/fields/Dropdown.jsx
@@ -41,34 +41,59 @@ function Dropdown({
             }}
         >
             {fieldLabel}
-            <ReactForm.Control
+            <div
                 css={{
-                    ...bootstrapStyles,
-                    ...applyStyles.getTarget('field'),
-                    ...(inlineError ? { borderColor: '#F42525' } : {}),
+                    position: 'relative',
                     width: '100%',
-                    appearance: 'none',
-                    WebkitAppearance: 'none',
-                    MozAppearance: 'none',
-                    backgroundImage:
-                        "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='6' viewBox='0 0 10 6' fill='none'><path d='M0 0.776454L0.970744 0L5 4.2094L9.02926 0L10 0.776454L5 6L0 0.776454Z' fill='black'/></svg>\")",
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'right 10px center',
-                    '&:focus': applyStyles.getTarget('active'),
-                    '&:hover': applyStyles.getTarget('hover')
+                    ...applyStyles.getTarget('sub-fc')
                 }}
-                as='select'
-                id={servar.key}
-                value={fieldVal}
-                required={servar.required}
-                onChange={onChange}
-                onClick={onClick}
             >
-                <option key='' value='' disabled>
+                <ReactForm.Control
+                    css={{
+                        ...bootstrapStyles,
+                        ...applyStyles.getTarget('field'),
+                        ...(inlineError ? { borderColor: '#F42525' } : {}),
+                        width: '100%',
+                        appearance: 'none',
+                        WebkitAppearance: 'none',
+                        MozAppearance: 'none',
+                        backgroundImage:
+                            "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='10' height='6' viewBox='0 0 10 6' fill='none'><path d='M0 0.776454L0.970744 0L5 4.2094L9.02926 0L10 0.776454L5 6L0 0.776454Z' fill='black'/></svg>\")",
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'right 10px center',
+                        '&:focus': applyStyles.getTarget('active'),
+                        '&:hover': applyStyles.getTarget('hover')
+                    }}
+                    as='select'
+                    id={servar.key}
+                    value={fieldVal}
+                    required={servar.required}
+                    onChange={onChange}
+                    onClick={onClick}
+                >
+                    <option key='' value='' disabled hidden />
+                    {options}
+                </ReactForm.Control>
+                <span
+                    css={{
+                        position: 'absolute',
+                        pointerEvents: 'none',
+                        left: '0.75rem',
+                        transition: '0.2s ease all',
+                        top: '50%',
+                        ...applyStyles.getTarget('placeholder'),
+                        ...(fieldVal
+                            ? applyStyles.getTarget('placeholderFocus')
+                            : {}),
+                        [`input:focus + &`]: {
+                            ...applyStyles.getTarget('placeholderFocus'),
+                            ...applyStyles.getTarget('placeholderActive')
+                        }
+                    }}
+                >
                     {field.placeholder || placeholder}
-                </option>
-                {options}
-            </ReactForm.Control>
+                </span>
+            </div>
             {inlineError && (
                 <span
                     css={{
