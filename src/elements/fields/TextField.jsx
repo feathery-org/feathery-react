20ceba383fe85d0635a1b07545c791ad13d8b826
bo-dun-1 <pdun2008@gmail.com>
2021-12-21 01:18:18 -0500

    dynamic mask support

--- src/elements/fields/TextField.jsx
+++ src/elements/fields/TextField.jsx
@@ -1,15 +1,49 @@
-import { IMaskMixin } from 'react-imask';
+import { IMaskInput } from 'react-imask';
 import React, { memo } from 'react';
-import ReactForm from 'react-bootstrap/Form';
+
 import InlineTooltip from '../components/Tooltip';
 import { bootstrapStyles } from '../styles';
 import { emailPatternStr } from '../../utils/formHelperFunctions';
 
-function getTextFieldProps(servar, styles, value) {
-  let methods, onlyPhone;
+const MAX_TEXT_FIELD_LENGTH = 512;
+
+function escapeDefinitionChars(str) {
+  return str
+    .replace('0', '\\0')
+    .replace('a', '\\a')
+    .replace('b', '\\b')
+    .replace('*', '\\*');
+}
+
+function constraintChar(allowed) {
+  switch (allowed) {
+    case 'letter':
+      return 'a';
+    case 'alphanumeric':
+      return 'b';
+    case 'digit':
+      return '0';
+    default:
+      return '*';
+  }
+}
+
+function getTextFieldMask(servar) {
+  const prefix = escapeDefinitionChars(servar.metadata.prefix || '');
+  const suffix = escapeDefinitionChars(servar.metadata.suffix || '');
+  const definitionChar = constraintChar(servar.metadata.allowed_characters);
+  let numOptional = MAX_TEXT_FIELD_LENGTH - prefix.length - suffix.length;
+  if (servar.max_length) numOptional = Math.min(servar.max_length, numOptional);
+
+  // Approximate dynamic input by making each character optional
+  return `${prefix}[${definitionChar.repeat(numOptional)}]${suffix}`;
+}
+
+function getMaskProps(servar, value) {
+  let methods, maskProps;
   switch (servar.type) {
     case 'integer_field':
-      return {
+      maskProps = {
         mask: servar.format === 'currency' ? '$num' : 'num',
         blocks: {
           num: {
@@ -19,59 +53,95 @@ function getTextFieldProps(servar, styles, value) {
             signed: false
           }
         },
-        value: value.toString(),
-        type: 'tel'
+        value: value.toString()
       };
+      break;
     case 'email':
-      return {
+      maskProps = {
         mask: /.+/,
-        type: 'email',
-        pattern: emailPatternStr,
         value
       };
+      break;
     case 'login':
       methods = servar.metadata.login_methods;
-      onlyPhone = methods.length === 1 && methods[0] === 'phone';
-      return {
+      maskProps = {
         mask: methods.map((method) => {
           return {
             method,
             mask: method === 'phone' ? '(000) 000-0000' : /.+/
           };
         }),
-        type: onlyPhone ? 'tel' : 'text',
         value
       };
+      break;
     case 'phone_number':
-      return {
+      maskProps = {
         mask: '(000) 000-0000',
-        type: 'tel',
         value
       };
+      break;
     case 'ssn':
-      return {
+      maskProps = {
         mask: '000 - 00 - 0000',
-        type: 'tel',
         value
       };
+      break;
+    case 'text_area':
+      maskProps = {
+        mask: /.+/,
+        value
+      };
+      break;
+    case 'url':
+      maskProps = {
+        mask: /.+/,
+        value
+      };
+      break;
+    default:
+      maskProps = {
+        mask: getTextFieldMask(servar),
+        definitions: {
+          b: /[a-zA-Z0-9]/
+        },
+        maxLength: MAX_TEXT_FIELD_LENGTH
+      };
+      break;
+  }
+  return {
+    ...maskProps,
+    lazy: false,
+    unmask: true
+  };
+}
+
+function getInputProps(servar, styles) {
+  let methods, onlyPhone;
+  switch (servar.type) {
+    case 'integer_field':
+      return { type: 'tel' };
+    case 'email':
+      return {
+        type: 'email',
+        pattern: emailPatternStr
+      };
+    case 'login':
+      methods = servar.metadata.login_methods;
+      onlyPhone = methods.length === 1 && methods[0] === 'phone';
+      return { type: onlyPhone ? 'tel' : 'text' };
+    case 'phone_number':
+      return { type: 'tel' };
+    case 'ssn':
+      return { type: 'tel' };
     case 'text_area':
       return {
-        mask: /.+/,
         as: 'textarea',
-        rows: styles.num_rows,
-        value
+        rows: styles.num_rows
       };
     case 'url':
-      return {
-        mask: /.+/,
-        type: 'url',
-        value
-      };
+      return { type: 'url' };
     default:
-      return {
-        mask: servar.metadata.only_alpha ? /^[a-z0-9]*$/i : /.*/,
-        value
-      };
+      return {};
   }
 }
 
@@ -80,17 +150,17 @@ function TextField({
   applyStyles,
   fieldLabel,
   required = false,
-  fieldValue = '',
   onBlur = () => {},
-  onChange = () => {},
   onClick = () => {},
   elementProps = {},
   inlineError,
-  inputRef,
-  ...fieldProps
+  rawValue = '',
+  onAccept = () => {}
 }) {
   const servar = element.servar;
-  const inputType = fieldProps.as === 'textarea' ? 'textarea' : 'input';
+
+  const inputProps = getInputProps(servar, element.styles);
+  const inputType = inputProps.as === 'textarea' ? 'textarea' : 'input';
   return (
     <div
       css={{
@@ -113,7 +183,7 @@ function TextField({
           ...applyStyles.getTarget('sub-fc')
         }}
       >
-        <ReactForm.Control
+        <IMaskInput
           id={servar.key}
           css={{
             height: '100%',
@@ -124,7 +194,7 @@ function TextField({
             '&:focus': applyStyles.getTarget('active'),
             '&:hover': applyStyles.getTarget('hover'),
             '&:not(:focus)':
-              fieldValue || !element.properties.placeholder
+              rawValue || !element.properties.placeholder
                 ? {}
                 : { color: 'transparent' }
           }}
@@ -132,12 +202,13 @@ function TextField({
           minLength={servar.min_length}
           required={required}
           onBlur={onBlur}
-          onChange={onChange}
           onClick={onClick}
           autoComplete={servar.metadata.autocomplete || 'on'}
-          ref={inputRef}
           placeholder=''
-          {...fieldProps}
+          defaultValue={rawValue}
+          {...inputProps}
+          {...getMaskProps(servar, rawValue)}
+          onAccept={onAccept}
         />
         <span
           css={{
@@ -147,7 +218,7 @@ function TextField({
             transition: '0.2s ease all',
             top: inputType === 'textarea' ? '0.375rem' : '50%',
             ...applyStyles.getTarget('placeholder'),
-            ...(fieldValue ? applyStyles.getTarget('placeholderFocus') : {}),
+            ...(rawValue ? applyStyles.getTarget('placeholderFocus') : {}),
             [`${inputType}:focus + &`]: {
               ...applyStyles.getTarget('placeholderFocus'),
               ...applyStyles.getTarget('placeholderActive')
@@ -168,19 +239,4 @@ function TextField({
   );
 }
 
-const MaskedTextField = IMaskMixin((props) => <TextField {...props} />);
-
-const MaskedPropsTextField = ({ element, fieldValue = '', ...props }) => {
-  const servar = element.servar;
-  const fieldProps = getTextFieldProps(servar, element.styles, fieldValue);
-  return (
-    <MaskedTextField
-      element={element}
-      fieldValue={fieldValue}
-      {...props}
-      {...fieldProps}
-    />
-  );
-};
-
-export default memo(MaskedPropsTextField);
+export default memo(TextField);
