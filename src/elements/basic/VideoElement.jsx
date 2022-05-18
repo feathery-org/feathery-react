0c447e5327f7aa293c10365e6b8341a55adc70b2
bo-dun-1 <pdun2008@gmail.com>
2022-05-18 14:36:49 -0700

    better video embed logic

--- src/elements/basic/VideoElement.jsx
+++ src/elements/basic/VideoElement.jsx
@@ -1,12 +1,20 @@
 import React, { useMemo } from 'react';
 
 const YOUTUBE_URL_REGEX = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+const VIMEO_URL_REGEX = /(?:http?s?:\/\/)?(?:www\.)?vimeo\.com(?:\/video)?\/?(.+)/;
 
 function getEmbedUrl(url) {
-  const match = url.match(YOUTUBE_URL_REGEX);
-  if (!match || match[2].length !== 11) return '';
-  const youtubeId = match[2];
-  return `https://www.youtube.com/embed/${youtubeId}`;
+  let match = url.match(YOUTUBE_URL_REGEX);
+  if (match && match[2].length === 11) {
+    return `https://www.youtube.com/embed/${match[2]}`;
+  }
+
+  match = url.match(VIMEO_URL_REGEX);
+  if (match && match[1]) {
+    return `https://player.vimeo.com/video/${match[1]}`;
+  }
+
+  return url;
 }
 
 function applyVideoStyles(element, applyStyles) {
