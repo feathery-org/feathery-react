ae4d32b6858b3469477b4514bdb55abc9a42df46
Peter Dun <46238013+bo-dun-1@users.noreply.github.com>
2022-08-01 11:48:42 -0700

    if no url specified, use placeholder image (#502)

--- src/elements/basic/ImageElement.jsx
+++ src/elements/basic/ImageElement.jsx
@@ -1,5 +1,8 @@
 import React, { useMemo } from 'react';
 
+const PLACEHOLDER_IMAGE =
+  'https://feathery.s3.us-west-1.amazonaws.com/theme-image-preview.png';
+
 function applyImageStyles(element, applyStyles) {
   applyStyles.addTargets('image');
   applyStyles.applyWidth('image');
@@ -7,13 +10,14 @@ function applyImageStyles(element, applyStyles) {
 }
 
 function ImageElement({ element, applyStyles, elementProps = {}, children }) {
-  const styles = useMemo(() => applyImageStyles(element, applyStyles), [
-    applyStyles
-  ]);
+  const styles = useMemo(
+    () => applyImageStyles(element, applyStyles),
+    [applyStyles]
+  );
   return (
     <div css={{ ...styles.getTarget('image'), position: 'relative' }}>
       <img
-        src={element.properties.source_image}
+        src={element.properties.source_image || PLACEHOLDER_IMAGE}
         alt='Form Image'
         css={{
           objectFit: 'contain',
