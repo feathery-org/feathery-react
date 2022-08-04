7befbb722b95d2a68c0bd57e8d528dbc1eb22ada
Peter Dun <46238013+bo-dun-1@users.noreply.github.com>
2022-08-04 16:16:30 -0700

    video placeholder graphic (#505)

--- src/elements/basic/VideoElement.jsx
+++ src/elements/basic/VideoElement.jsx
@@ -1,7 +1,12 @@
 import React, { useMemo } from 'react';
 
-const YOUTUBE_URL_REGEX = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-const VIMEO_URL_REGEX = /(?:http?s?:\/\/)?(?:www\.)?vimeo\.com(?:\/video)?\/?(.+)/;
+const PLACEHOLDER_VIDEO =
+  'https://feathery.s3.us-west-1.amazonaws.com/video-preview.png';
+
+const YOUTUBE_URL_REGEX =
+  /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+const VIMEO_URL_REGEX =
+  /(?:http?s?:\/\/)?(?:www\.)?vimeo\.com(?:\/video)?\/?(.+)/;
 
 function getEmbedUrl(url) {
   let match = url.match(YOUTUBE_URL_REGEX);
@@ -25,18 +30,38 @@ function applyVideoStyles(element, applyStyles) {
 }
 
 function VideoElement({ element, applyStyles, elementProps = {}, children }) {
-  const styles = useMemo(() => applyVideoStyles(element, applyStyles), [
-    applyStyles
-  ]);
+  const styles = useMemo(
+    () => applyVideoStyles(element, applyStyles),
+    [applyStyles]
+  );
   return (
-    <div css={{ ...styles.getTarget('video'), position: 'relative' }}>
-      <iframe
-        width='100%'
-        height='100%'
-        frameBorder='0'
-        src={getEmbedUrl(element.properties.source_url)}
-        {...elementProps}
-      />
+    <div
+      css={{
+        ...styles.getTarget('video'),
+        position: 'relative',
+        maxHeight: '100%'
+      }}
+    >
+      {element.properties.source_url ? (
+        <iframe
+          width='100%'
+          height='100%'
+          frameBorder='0'
+          src={getEmbedUrl(element.properties.source_url)}
+          {...elementProps}
+        />
+      ) : (
+        <img
+          src={PLACEHOLDER_VIDEO}
+          alt='Video Placeholder'
+          css={{
+            objectFit: 'contain',
+            width: '100%',
+            maxHeight: '100%'
+          }}
+          {...elementProps}
+        />
+      )}
       {children}
     </div>
   );
