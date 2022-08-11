62ac74e60219e27ebbff8710d57aa9e2187e9239
bo-dun-1 <pdun2008@gmail.com>
2022-08-11 10:40:49 -0700

    bundle analyzer

--- webpack.development.js
+++ webpack.development.js
@@ -1,11 +1,16 @@
 const config = require('./webpack.config');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 config.devtool = 'eval-cheap-module-source-map';
 config.externals = ['react'];
 config.plugins.push(
   new webpack.optimize.LimitChunkCountPlugin({
     maxChunks: 1
-  })
+  }),
+  // Set openAnalyzer to true to view interactive bundle size breakdown
+  new BundleAnalyzerPlugin({ defaultSizes: 'stat', openAnalyzer: false })
 );
+
 module.exports = config;
