2b8c6ba25b366e79fad3fa3def3e14292b4083b9
bo-dun-1 <pdun2008@gmail.com>
2022-08-15 16:30:31 -0700

    dont initialize bundle analyzer if not opening

--- webpack.development.js
+++ webpack.development.js
@@ -1,16 +1,19 @@
 const config = require('./webpack.config');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin =
-  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 config.devtool = 'eval-cheap-module-source-map';
 config.externals = ['react'];
 config.plugins.push(
   new webpack.optimize.LimitChunkCountPlugin({
     maxChunks: 1
-  }),
-  // Set openAnalyzer to true to view interactive bundle size breakdown
-  new BundleAnalyzerPlugin({ defaultSizes: 'stat', openAnalyzer: false })
+  })
 );
+
+// Uncomment to view interactive bundle size breakdown
+// const BundleAnalyzerPlugin =
+//   require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// config.plugins.push(
+//   new BundleAnalyzerPlugin({ defaultSizes: 'stat', openAnalyzer: true })
+// );
 
 module.exports = config;
