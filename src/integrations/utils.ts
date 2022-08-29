bb2feffecad5a10d58847fd88765916022ea17e9
Peter Dun <46238013+bo-dun-1@users.noreply.github.com>
2022-08-29 08:19:47 -0700

    check if stytch or fb are loaded before inferring email login (#539)

--- src/integrations/utils.ts
+++ src/integrations/utils.ts
@@ -64,7 +64,7 @@ export async function initializeIntegrations(
   ]);
 
   if (gtm) initializeTagManager(gtm);
-  inferEmailLoginFromURL(clientArg);
+  if (fb || stytch) inferEmailLoginFromURL(clientArg);
 }
 
 export function inferEmailLoginFromURL(featheryClient: any) {
