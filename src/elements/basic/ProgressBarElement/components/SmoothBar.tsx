674de5eee7e2b0f0cc8d0f8e65a6153b63b61cf5
bo-dun-1 <pdun2008@gmail.com>
2022-11-22 17:23:36 -0800

    support segment progress bar

---
+++ src/elements/basic/ProgressBarElement/components/SmoothBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function SmoothBar({ styles, percent }: any) {
+  return (
+    <div
+      style={{
+        height: '0.4rem',
+        width: '100%',
+        borderRadius: 0,
+        display: 'flex',
+        backgroundColor: '#e9ecef'
+      }}
+    >
+      <div
+        style={{ width: `${percent}%` }}
+        css={{
+          transition: 'width 0.6s ease',
+          ...styles.getTarget('bar')
+        }}
+      />
+    </div>
+  );
+}
+
+export default SmoothBar;
