f757f3ce4a807118d0502f50d024437ec22498b6
Peter Dun <46238013+bo-dun-1@users.noreply.github.com>
2022-12-14 00:22:14 -0800

    allow pulling file for image from field (#654)

--- src/elements/basic/ImageElement.tsx
+++ src/elements/basic/ImageElement.tsx
@@ -1,4 +1,6 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { fieldValues } from '../../utils/init';
+import { getThumbnailData } from '../../utils/image';
 
 const PLACEHOLDER_IMAGE =
   'https://feathery.s3.us-west-1.amazonaws.com/theme-image-preview.png';
@@ -17,10 +19,25 @@ function ImageElement({
   elementProps = {},
   children
 }: any) {
+  const imageField = element.properties.uploaded_image_file_field_key ?? '';
+  let imageFieldFile = fieldValues[imageField] as any[] | undefined;
+  if (imageFieldFile) imageFieldFile = imageFieldFile[0];
+
+  const [imageUrl, setImageUrl] = useState(element.properties.source_image);
+
   const styles = useMemo(
     () => applyImageStyles(element, responsiveStyles),
     [responsiveStyles]
   );
+
+  useEffect(() => {
+    if (imageFieldFile)
+      getThumbnailData(imageFieldFile).then((data) =>
+        setImageUrl(data.thumbnail)
+      );
+    else setImageUrl(element.properties.source_image);
+  }, [imageFieldFile]);
+
   return (
     <div
       css={{
@@ -32,17 +49,19 @@ function ImageElement({
       }}
     >
       {children}
-      <img
-        src={element.properties.source_image || PLACEHOLDER_IMAGE}
-        alt='Form Image'
-        css={{
-          objectFit: 'contain',
-          width: '100%',
-          maxHeight: '100%',
-          ...styles.getTarget('image')
-        }}
-        {...elementProps}
-      />
+      {imageField && !imageUrl ? null : (
+        <img
+          src={imageUrl || PLACEHOLDER_IMAGE}
+          alt='Form Image'
+          css={{
+            objectFit: 'contain',
+            width: '100%',
+            maxHeight: '100%',
+            ...styles.getTarget('image')
+          }}
+          {...elementProps}
+        />
+      )}
     </div>
   );
 }
