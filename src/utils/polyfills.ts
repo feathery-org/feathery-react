7589dc0e1b973214ee80c636218cb85bc4174cf2
bo-dun-1 <pdun2008@gmail.com>
2023-03-31 09:13:20 -0700

    account for regex like characters

--- src/utils/polyfills.ts
+++ src/utils/polyfills.ts
@@ -1,3 +1,16 @@
+/**
+ * Used to match `RegExp`
+ * [syntax characters](http://ecma-international.org/ecma-262/7.0/#sec-patterns).
+ */
+const reRegExpChar = /[\\^$.*+?()[\]{}|]/g;
+const reHasRegExpChar = RegExp(reRegExpChar.source);
+
+function escapeRegExp(str: string) {
+  return str && reHasRegExpChar.test(str)
+    ? str.replace(reRegExpChar, '\\$&')
+    : str;
+}
+
 export default function polyfill() {
   if (!String.prototype.replaceAll) {
     // eslint-disable-next-line no-extend-native
@@ -7,10 +20,13 @@ export default function polyfill() {
         Object.prototype.toString.call(str).toLowerCase() === '[object regexp]'
       ) {
         return this.replace(str, newStr);
+      } else {
+        // If a string
+        return this.replace(
+          new RegExp(escapeRegExp(str as string), 'g'),
+          newStr
+        );
       }
-
-      // If a string
-      return this.replace(new RegExp(str, 'g'), newStr);
     };
   }
 }
