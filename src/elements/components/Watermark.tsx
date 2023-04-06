276fb94c86f772825acfec39c3fbf77548099ce3
Peter Dun <46238013+bo-dun-1@users.noreply.github.com>
2023-04-06 15:38:55 -0700

    wrap forms in anchor tag (#794)

--- src/elements/components/Watermark.tsx
+++ src/elements/components/Watermark.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { openTab } from '../../utils/browser';
 
 export default function Watermark({
+  show,
   addChin,
   brandPosition = 'bottom_right',
   width = 135,
@@ -13,7 +13,22 @@ export default function Watermark({
   const verticalAlignment = brandPosition.startsWith('bottom')
     ? { bottom: addChin ? -60 : 0 }
     : { top: 0 };
-  return (
+
+  const anchorWrap = (children?: any) => {
+    return (
+      /* eslint-disable-next-line react/jsx-no-target-blank  */
+      <a
+        href='https://feathery.io'
+        target='_blank'
+        css={{ textDecoration: 'none' }}
+        rel='noopener'
+      >
+        {children}
+      </a>
+    );
+  };
+
+  return show ? (
     <div
       css={{
         position: 'absolute',
@@ -21,35 +36,38 @@ export default function Watermark({
         ...verticalAlignment
       }}
     >
-      <div
-        css={{
-          width: `${width}px`,
-          height: `${height}px`,
-          marginRight: '5px',
-          marginBottom: '5px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          background: '#FFFFFF',
-          boxShadow: '0px 2px 10px rgba(151, 161, 172, 0.3)',
-          borderRadius: '6px',
-          color: '#6c7589',
-          fontFamily: 'Axiforma, sans-serif',
-          fontSmoothing: 'antialiased',
-          fontWeight: 400,
-          fontSize: '11px',
-          gap: '8px',
-          cursor: 'pointer'
-        }}
-        onClick={() => openTab('https://feathery.io')}
-      >
-        Built on
-        <img
-          css={{ maxWidth: '50%', maxHeight: '80%', paddingBottom: '1px' }}
-          src='https://feathery.s3.us-west-1.amazonaws.com/full-logo-1.png'
-          alt='Feathery Logo'
-        />
-      </div>
+      {anchorWrap(
+        <div
+          css={{
+            width: `${width}px`,
+            height: `${height}px`,
+            marginRight: '5px',
+            marginBottom: '5px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: '#FFFFFF',
+            boxShadow: '0px 2px 10px rgba(151, 161, 172, 0.3)',
+            borderRadius: '6px',
+            color: '#6c7589',
+            fontFamily: 'Axiforma, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontWeight: 400,
+            fontSize: '11px',
+            gap: '8px',
+            cursor: 'pointer'
+          }}
+        >
+          Built on
+          <img
+            css={{ maxWidth: '50%', maxHeight: '80%', paddingBottom: '1px' }}
+            src='https://feathery.s3.us-west-1.amazonaws.com/full-logo-1.png'
+            alt='Feathery Logo'
+          />
+        </div>
+      )}
     </div>
+  ) : (
+    anchorWrap()
   );
 }
