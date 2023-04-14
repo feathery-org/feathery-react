d4a64c3f40f53bd192776c2a13dea87f2f596ee2
bo-dun-1 <pdun2008@gmail.com>
2023-04-13 23:44:25 -0700

    add flatmap polyfill

--- src/utils/polyfills.ts
+++ src/utils/polyfills.ts
@@ -1,3 +1,5 @@
+import 'flat-map-polyfill';
+
 /**
  * Used to match `RegExp`
  * [syntax characters](http://ecma-international.org/ecma-262/7.0/#sec-patterns).
@@ -11,22 +13,19 @@ function escapeRegExp(str: string) {
     : str;
 }
 
-export default function polyfill() {
-  if (!String.prototype.replaceAll) {
-    // eslint-disable-next-line no-extend-native
-    String.prototype.replaceAll = function (str, newStr: any) {
-      // If a regex pattern
-      if (
-        Object.prototype.toString.call(str).toLowerCase() === '[object regexp]'
-      ) {
-        return this.replace(str, newStr);
-      } else {
-        // If a string
-        return this.replace(
-          new RegExp(escapeRegExp(str as string), 'g'),
-          newStr
-        );
-      }
-    };
-  }
+if (!String.prototype.replaceAll) {
+  // eslint-disable-next-line no-extend-native
+  String.prototype.replaceAll = function (str, newStr: any) {
+    // If a regex pattern
+    if (
+      Object.prototype.toString.call(str).toLowerCase() === '[object regexp]'
+    ) {
+      return this.replace(str, newStr);
+    } else {
+      // If a string
+      return this.replace(new RegExp(escapeRegExp(str as string), 'g'), newStr);
+    }
+  };
 }
+
+export {};
