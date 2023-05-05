9ce8d8c6a54414d8226772ebc25ee1171847caed
Peter Dun <46238013+bo-dun-1@users.noreply.github.com>
2023-05-05 13:33:23 -0400

    fix argyle environment url (#821)

--- src/integrations/argyle.ts
+++ src/integrations/argyle.ts
@@ -22,7 +22,7 @@ export async function openArgyleLink(
   const userToken = (await client.fetchArgyleUserToken()).user_token;
   const argyle = global.Argyle.create({
     linkKey: argyleConfig.metadata.link_key,
-    apiHost: argyleConfig.metadata.environment_url,
+    apiHost: argyleConfig.metadata.environment,
     userToken,
     onAccountConnected: async ({ linkItemId }: { linkItemId: string }) => {
       const fieldVals = await client.submitArgyleUserData(linkItemId);
