592cf01e806b8849b1f653fe7a0695f4b12f9f3b
bo-dun-1 <pdun2008@gmail.com>
2023-05-10 23:29:41 -0700

    address background dropdown color

--- src/elements/fields/AddressLine1.tsx
+++ src/elements/fields/AddressLine1.tsx
@@ -83,7 +83,8 @@ function AddressLine1({
                 margin: 0,
                 backgroundColor: 'white',
                 cursor: 'pointer',
-                boxShadow: '0 0 4px rgb(0 0 0 / 15%)'
+                boxShadow: '0 0 4px rgb(0 0 0 / 15%)',
+                ...responsiveStyles.getTarget('dropdown')
               }}
             >
               {options.map(({ display }) => (
