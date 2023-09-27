8996f6eabf9adc86433af53e46c413e3293a3551
Eric <ericdpias@gmail.com>
2023-09-27 14:27:03 -0400

    Fixes renderAt returned context fields prop to have fields in it (#1025)

--- src/utils/internalState.ts
+++ src/utils/internalState.ts
@@ -1,36 +1,69 @@
 import { FieldOptions } from './formHelperFunctions';
 import Field from './Field';
+import co from '../elements/components/data/states/co';
 
+interface FormInternalState {
+  currentStep: any;
+  previousStepName: string;
+  visiblePositions: any;
+  client: any;
+  fields: Record<string, Field>;
+  formName: string;
+  formRef: React.MutableRefObject<any>;
+  formSettings: any;
+  getErrorCallback: (
+    props1?: Record<string, unknown>
+  ) => (props2?: Record<string, unknown>) => Promise<boolean>;
+  history: any;
+  inlineErrors: Record<string, { message: string; index: number }>;
+  setInlineErrors: React.Dispatch<
+    React.SetStateAction<Record<string, { message: string; index: number }>>
+  >;
+  setUserProgress: React.Dispatch<React.SetStateAction<null>>;
+  steps: any;
+  updateFieldOptions: (
+    stepData: any,
+    loadStep?: null
+  ) => (newOptions: FieldOptions) => void;
+  setFieldErrors: (
+    errors: Record<string, string | { index: number; message: string }>
+  ) => void;
+}
 type InternalState = {
-  [formUuid: string]: {
-    currentStep: any;
-    previousStepName: string;
-    visiblePositions: any;
-    client: any;
-    fields: Record<string, Field>;
-    formName: string;
-    formRef: React.MutableRefObject<any>;
-    formSettings: any;
-    getErrorCallback: (
-      props1?: Record<string, unknown>
-    ) => (props2?: Record<string, unknown>) => Promise<boolean>;
-    history: any;
-    inlineErrors: Record<string, { message: string; index: number }>;
-    setInlineErrors: React.Dispatch<
-      React.SetStateAction<Record<string, { message: string; index: number }>>
-    >;
-    setUserProgress: React.Dispatch<React.SetStateAction<null>>;
-    steps: any;
-    updateFieldOptions: (
-      stepData: any,
-      loadStep?: null
-    ) => (newOptions: FieldOptions) => void;
-    setFieldErrors: (
-      errors: Record<string, string | { index: number; message: string }>
-    ) => void;
-  };
+  [formUuid: string]: FormInternalState;
 };
 
 const internalState: InternalState = {};
+
+// Function that will take a formUuid string and create a new internalState object for the form.
+// It also takes an object of stateValues to set on the internalState object for the form
+export const setFormInternalState = (
+  formUuid: string,
+  stateValues: Record<string, any>
+) => {
+  // if the formUuid is already in the internalState object, use it
+  const state = internalState[formUuid] ?? {};
+
+  // now overlay each of the stateValues entries onto the corresponding property in the internalState object using Object.assign
+  Object.entries(stateValues).forEach(([key, value]) => {
+    // if the key in the state object is null or undefined or not and object,
+    // then set it to the value from the stateValues object.
+    // Otherwise, overlay the obj value from the stateValues object onto the obj value in the state object using Object.assign
+    const stateKey: keyof FormInternalState = key as keyof FormInternalState;
+    if (
+      state[stateKey] === undefined ||
+      state[stateKey] === null ||
+      typeof state[stateKey] !== 'object'
+    )
+      state[stateKey] = value;
+    else {
+      Object.assign(state[stateKey], value);
+    }
+  });
+
+  // return the form's internalState object
+  internalState[formUuid] = state;
+  return internalState[formUuid];
+};
 
 export default internalState;
