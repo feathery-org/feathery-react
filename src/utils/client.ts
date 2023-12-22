d199747a1583bfe55ef58e3b7e6f60c38617c407
ondrosh <mail@andras.io>
2023-12-22 13:39:15 -0500

    wip: Bump panel endpoint to 20

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -310,7 +310,7 @@ export default class Client {
       theme
     });
     const baseURL = this.bypassCDN ? API_URL : CDN_URL;
-    const url = `${baseURL}panel/v19/?${params}`;
+    const url = `${baseURL}panel/v20/?${params}`;
     const options: Record<string, any> = {
       importance: 'high',
       headers: { 'Accept-Encoding': 'gzip' }
