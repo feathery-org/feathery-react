b867dd406e437c90c948e83e6eeab99c0d3de583
bo-dun-1 <pdun2008@gmail.com>
2024-01-08 23:26:16 -0800

    updated quik document action

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -850,29 +850,13 @@ export default class Client {
     });
   }
 
-  qwikDocuments() {
-    const { _internalUserId } = initInfo();
-    const url = `${API_URL}qwik/document/`;
+  quikDocuments(jsonKey: string) {
+    const formPayload = fieldValues[jsonKey] as string;
+    const url = `${API_URL}quik/document/`;
     const options = {
       headers: { 'Content-Type': 'application/json' },
       method: 'POST',
-      body: JSON.stringify({
-        FormFields: [
-          {
-            FieldName: '1own.FName',
-            FieldValue: fieldValues.FullName
-          },
-          {
-            FieldName: '1own.H.Addr123',
-            FieldValue: fieldValues.ClientAddress
-          },
-          { FieldName: '1own.SSN', FieldValue: fieldValues.SSN },
-          { FieldName: '1own.H.Email', FieldValue: fieldValues.ClientEmail }
-        ],
-        QuikFormIDs: ['87154'],
-        fuserId: _internalUserId,
-        tags: [[fieldValues.Brokerage]]
-      })
+      body: formPayload
     };
     return fetch(url, options).then(async (response) => {
       if (response) {
