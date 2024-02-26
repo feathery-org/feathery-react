6e8e92be649f8d3c24cdf5e312e3b1b9b00b7206
Summer Pu <124018018+summep@users.noreply.github.com>
2024-02-26 07:57:13 -0800

    Added SMS Code and Telesign Voice No Code Actions (#1146)

---
+++ src/utils/sensitiveActions.ts
@@ -0,0 +1,23 @@
+import internalState, { setFormInternalState } from './internalState';
+import Auth from '../auth/internal/AuthIntegrationInterface';
+
+// Certain actions are only supported in the no code logic rule
+// And we do not want to expose them in formContext for SDK access
+export const getPrivateActions = (formUuid: string) => {
+  if (!internalState[formUuid]) {
+    setFormInternalState(formUuid, {
+      fields: {}
+    });
+  }
+
+  return {
+    _sendSmsCode: (phoneNumber: string) => {
+      const { client } = internalState[formUuid];
+      return Auth.sendSms(phoneNumber, client);
+    },
+    _telesignVoice: (phoneNumber: string) => {
+      const { client } = internalState[formUuid];
+      return client.telesignVoiceOTP(phoneNumber);
+    }
+  };
+};
