c4c4a30186116b69e2e1dd65423b5314245eb1dc
summep <>
2024-03-18 11:34:03 -0700

    further updated

--- src/utils/offlineRequestHandler.ts
+++ src/utils/offlineRequestHandler.ts
@@ -72,6 +72,8 @@ export function useOfflineRequestHandler(formKey: string) {
     featheryWindow().addEventListener('online', handleOnline);
     return () => featheryWindow().removeEventListener('online', handleOnline);
   }, []);
+
+  offlineRequestHandler.ignoreNetworkErrors = hasRedirected;
 }
 
 export class OfflineRequestHandler {
@@ -365,6 +367,30 @@ export class OfflineRequestHandler {
   private async removeRequest() {
     const { store } = await this.getDbTransaction('readwrite');
     await store.clear();
+  }
+
+  private async checkResponseSuccess(response: any) {
+    let payload;
+    switch (response.status) {
+      case 200:
+      case 201:
+        return;
+      case 400:
+        payload = JSON.stringify(await response.clone().text());
+        console.error(payload.toString());
+        return;
+      case 401:
+        throw new errors.SDKKeyError();
+      case 404:
+        throw new errors.FetchError("Can't find object");
+      case 409:
+        location.reload();
+        return;
+      case 500:
+        throw new errors.FetchError('Internal server error');
+      default:
+        throw new errors.FetchError('Unknown error');
+    }
   }
 
   private delay(ms: number): Promise<void> {
