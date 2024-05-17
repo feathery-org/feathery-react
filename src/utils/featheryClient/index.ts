35de89b472bdae806379c6d990d97ee60642645f
Peter D <pdun2008@gmail.com>
2024-05-16 17:13:25 -0700

    disable no save from beginning flag

--- src/utils/featheryClient/index.ts
+++ src/utils/featheryClient/index.ts
@@ -328,7 +328,7 @@ export default class FeatheryClient extends IntegrationClient {
     // If form is disabled, data will equal `null`
     if (!res.steps) return { steps: [], formOff: true };
     this.version = res.version;
-    this.noSave = res.no_save_data;
+    this._noSave = res.no_save_data;
     this.setDefaultFormValues({ steps: res.steps, additionalValues: initVals });
     return res;
   }
@@ -555,6 +555,11 @@ export default class FeatheryClient extends IntegrationClient {
     );
   }
 
+  getNoSave() {
+    if (this._noSave !== undefined) return this._noSave;
+    return initInfo().initNoSave;
+  }
+
   async submitCustom(
     customKeyValues: { [key: string]: any },
     // Options
@@ -563,7 +568,7 @@ export default class FeatheryClient extends IntegrationClient {
       shouldFlush = false
     }: { override?: boolean; shouldFlush?: boolean } = {}
   ) {
-    if (this.draft || this.noSave) return;
+    if (this.draft || this.getNoSave()) return;
     if (Object.keys(customKeyValues).length === 0 && !shouldFlush) return;
     // If there are values passed, aggregate them in the pending queue
     Object.entries(customKeyValues).forEach(([key, value]) => {
@@ -583,7 +588,7 @@ export default class FeatheryClient extends IntegrationClient {
 
   // servars = [{key: <servarKey>, <type>: <value>}]
   async submitStep(servars: any, step: any, hasNext: boolean) {
-    if (this.draft || this.noSave) return;
+    if (this.draft || this.getNoSave()) return;
 
     const items = [
       ...step.buttons.filter(isStoreFieldValueAction),
