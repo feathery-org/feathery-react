1c296f72f8c17f53d9c4d1be0d90ee5eaec8edab
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2024-07-18 15:49:05 -0500

    QR Code Scanner Reimplementation (#1255)

---
+++ src/elements/fields/QRScanner/utils/supports-zoom.ts
@@ -0,0 +1,43 @@
+type ZoomSettings = {
+  min: number;
+  max: number;
+  current: number;
+  step: number;
+};
+
+// TODO: figure out how to type scanner
+export function getZoomSettings(scanner: any): ZoomSettings | null {
+  if (!scanner) {
+    return null;
+  }
+
+  // possible options
+  const cameraCapabilities = scanner.getRunningTrackCapabilities();
+  // current options
+  const cameraSettings = scanner.getRunningTrackSettings();
+
+  // todo: figure out type for cameraCapabilities
+  const supportsZoom =
+    'zoom' in cameraCapabilities &&
+    'min' in (cameraCapabilities.zoom as any) &&
+    'max' in (cameraCapabilities.zoom as any);
+
+  if (!supportsZoom) {
+    return null;
+  }
+
+  const zoomCapabilities = cameraCapabilities.zoom as any;
+
+  const current = (
+    'zoom' in cameraSettings ? cameraSettings.zoom : 1
+  ) as number;
+
+  const zoomSettings = {
+    min: zoomCapabilities.min as number,
+    max: zoomCapabilities.max as number,
+    step: zoomCapabilities.step ?? 0.1,
+    current
+  };
+
+  return zoomSettings;
+}
