b3e828da767356b43df977698f60ddca3ca55d2f
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2024-07-18 13:49:57 -0500

    adjust comment

--- src/elements/fields/QRScanner/utils/select-camera.ts
+++ src/elements/fields/QRScanner/utils/select-camera.ts
@@ -1,8 +1,8 @@
 /*
  * selects the best camera available on the device to scan QR code
  * currently the way it does this is by taking the last camera in the list of video devices
- * according to the internet, most devices have the 'normal' back camera as the last device in the list
- * this was the most reliable to pick the back camera, and not secondary back cameras like the wide angle one
+ * every devices has the 'normal' back camera as the last device in the list
+ * this is the most reliable to pick the back camera, and not secondary back cameras like the wide angle one
  */
 export async function selectCamera(): Promise<any> {
   const devices = await fetchDeviceList();
