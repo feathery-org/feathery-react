9890de414dbe0251e327a216de5ffa8062c176a4
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2024-07-30 11:53:16 -0500

    fix: datepicker modal positioning (#1260)

--- src/elements/fields/DateSelectorField/styles.tsx
+++ src/elements/fields/DateSelectorField/styles.tsx
@@ -902,9 +902,9 @@ export default function SelectorStyles() {
 
         .react-datepicker__portal {
           position: fixed;
-          width: 100vw;
-          height: 100vh;
           background-color: rgba(0, 0, 0, 0.8);
+          bottom: 0;
+          right: 0;
           left: 0;
           top: 0;
           justify-content: center;
