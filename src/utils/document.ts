8fbad4b243c098d213e9a508ec902f29844be330
Peter D <pdun2008@gmail.com>
2024-08-02 10:22:14 -0700

    fix document region url

--- src/utils/document.ts
+++ src/utils/document.ts
@@ -1,6 +1,7 @@
 import { initState } from './init';
 
 export function getSignUrl() {
-  const regionPart = initState.region ? `${initState.region}.` : '';
+  const regionPart =
+    initState.region && initState.region !== 'us' ? `${initState.region}.` : '';
   return `https://${regionPart}document.feathery.io/to/${initState._internalUserId}`;
 }
