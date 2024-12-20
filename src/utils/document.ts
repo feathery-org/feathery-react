922a3e42d4ca3c148e2417dfa0909c9979c4c127
Peter Dun <46238013+bo-dun-1@users.noreply.github.com>
2024-12-19 22:40:53 -0800

    fix: Guarantee document redirect maintains the same user session (#1319)

--- src/utils/document.ts
+++ src/utils/document.ts
@@ -1,10 +1,16 @@
-import { initState } from './init';
+import { initInfo, initState } from './init';
 
 export function getSignUrl(redirect?: boolean) {
   const regionPart =
     initState.region && initState.region !== 'us' ? `${initState.region}.` : '';
+
+  const url = new URL(location.href);
+  if (!url.searchParams.has('_id')) {
+    url.searchParams.append('_id', initInfo().userId ?? '');
+  }
   const query = redirect
-    ? `?redirect=${encodeURIComponent(location.href)}`
+    ? `?redirect=${encodeURIComponent(url.toString())}`
     : '';
+
   return `https://${regionPart}document.feathery.io/to/${initState._internalUserId}${query}`;
 }
