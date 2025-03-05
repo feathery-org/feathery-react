229769529d9e34f1bc8841d43e0761163e830ced
Peter Dun <pdun2008@gmail.com>
2025-03-04 16:07:12 -0800

    fix: Schwab create contact trigger

--- src/integrations/schwab.ts
+++ src/integrations/schwab.ts
@@ -1,5 +1,5 @@
 export async function createSchwabContact(client: any, setElementError: any) {
-  const res = await client.createSchwabContact();
+  const res = await client.schwabCreateContact();
   if (!res.ok) setElementError(res.error);
   else {
     location.href = 'https://advisorservices.schwab.com';
