60691dde2286be87c2a34cb05cdc36d3cccc4a11
Peter Dun <pdun2008@gmail.com>
2025-03-11 14:44:11 -0700

    refactor: Redirect to schwab account management view

--- src/integrations/schwab.ts
+++ src/integrations/schwab.ts
@@ -2,6 +2,7 @@ export async function createSchwabContact(client: any, setElementError: any) {
   const res = await client.schwabCreateContact();
   if (!res.ok) setElementError(res.error);
   else {
-    location.href = 'https://advisorservices.schwab.com';
+    location.href =
+      'https://si2.schwabinstitutional.com/SI2/Home/Utilities/AccountManagement.aspx';
   }
 }
