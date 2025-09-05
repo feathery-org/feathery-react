242fb1dfcc9dbe6c3f6d7ce9f357e43cce64a3d1
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2025-09-05 14:35:10 -0500

    fix: Webpack config

--- webpack.umd.js
+++ webpack.umd.js
@@ -1,16 +1,10 @@
 const path = require('path');
 const config = require('./webpack.config');
 
-config.output.filename = '[name].js';
 config.output.path = path.resolve(__dirname, 'umd');
 config.performance = {
   maxEntrypointSize: 1024000,
   maxAssetSize: 1024000
-};
-config.optimization = {
-  splitChunks: {
-    chunks: 'all'
-  }
 };
 
 module.exports = config;
