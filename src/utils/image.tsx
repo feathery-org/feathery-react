99d38832afdc19c399e31fe9939c37f0cde2ed3a
Joon Park <joon@feathery.io>
2025-10-14 13:57:12 -0400

    chore: Clean up removed extra null type

--- src/utils/image.tsx
+++ src/utils/image.tsx
@@ -47,9 +47,7 @@ export function useFileData(initialFiles: any, onSetFiles = () => {}) {
  * Given a File (or a Promise<File>), convert the file to a filename and thumbnail.
  * Filename will be a plaintext string and thumbnail will be a base64 encoded image.
  */
-export async function getThumbnailData(
-  filePromise: File | Promise<File> | null
-) {
+export async function getThumbnailData(filePromise: File | Promise<File>) {
   const file = await (filePromise as any);
   const thumbnailType = getThumbnailType(file);
   if (thumbnailType === THUMBNAIL_TYPE.IMAGE) {
@@ -86,7 +84,7 @@ export async function getRenderData(filePromise: File | Promise<File>) {
 /**
  * Utility hook for converting a list of files into a list of thumbnail information.
  */
-export function useThumbnailData(files: (Promise<File> | null)[]) {
+export function useThumbnailData(files: Promise<File>[]) {
   const [thumbnailData, setThumbnailData] = useState(
     files.map(() => ({ filename: '', thumbnail: '' }))
   );
