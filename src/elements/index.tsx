02e052ea4a71a1df74419f6cb941eb07ea4c972d
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2025-11-11 13:08:00 -0600

    chore: Polish and cleanup

--- src/elements/index.tsx
+++ src/elements/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, memo, useMemo, Suspense } from 'react';
+import React, { memo, useMemo, Suspense } from 'react';
 import { InView } from 'react-intersection-observer';
 
 import Fields from './fields';
@@ -9,50 +9,6 @@ import TextElement from './basic/TextElement';
 import ButtonElement from './basic/ButtonElement';
 import ProgressBarElement from './basic/ProgressBarElement';
 import FieldSkeleton from './components/skeletons/FieldSkeleton';
-
-// Set to a number (in ms) to artificially delay lazy loading for testing fallback UI
-const ARTIFICIAL_DELAY = 5000;
-
-export const delayImport = <T,>(importFn: () => Promise<T>): Promise<T> => {
-  if (ARTIFICIAL_DELAY > 0) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        importFn().then(resolve);
-      }, ARTIFICIAL_DELAY);
-    });
-  }
-  return importFn();
-};
-
-// const TextElement = lazy(() =>
-//   delayImport(
-//     () => import(/* webpackChunkName: "TextElement" */ './basic/TextElement')
-//   )
-// );
-// const ButtonElement = lazy(() =>
-//   delayImport(
-//     () =>
-//       import(/* webpackChunkName: "ButtonElement" */ './basic/ButtonElement')
-//   )
-// );
-// const ImageElement = lazy(() =>
-//   delayImport(
-//     () => import(/* webpackChunkName: "ImageElement" */ './basic/ImageElement')
-//   )
-// );
-// const ProgressBarElement = lazy(() =>
-//   delayImport(
-//     () =>
-//       import(
-//         /* webpackChunkName: "ProgressBarElement" */ './basic/ProgressBarElement'
-//       )
-//   )
-// );
-// const VideoElement = lazy(() =>
-//   delayImport(
-//     () => import(/* webpackChunkName: "VideoElement" */ './basic/VideoElement')
-//   )
-// );
 
 const Basic = {
   ImageElement,
