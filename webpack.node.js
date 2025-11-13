742e8f725a027a4c6641d84dbc0c57f1ba51e23f
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2025-11-13 13:28:06 -0600

    fix: Force ESM build to use a single chunk

--- webpack.node.js
+++ webpack.node.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const config = require('./webpack.config');
+const webpack = require('webpack');
 
 module.exports = (env) => {
   if (env.analyze) {
@@ -21,5 +22,10 @@ module.exports = (env) => {
     maxEntrypointSize: 1024000,
     maxAssetSize: 1024000
   };
+  config.plugins.push(
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  );
   return config;
 };
