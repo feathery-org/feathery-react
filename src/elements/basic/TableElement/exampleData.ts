d06525b80ef73339fdac214537837c33ef9731ab
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2025-12-10 12:27:16 -0500

    feat: Default 2 rows

--- src/elements/basic/TableElement/exampleData.ts
+++ src/elements/basic/TableElement/exampleData.ts
@@ -2,16 +2,12 @@ import { Column } from './types';
 
 export function generateExampleData(
   columns: Column[],
-  numRows = 1
+  numRows = 2
 ): Record<string, any[]> {
   const exampleData: Record<string, any[]> = {};
 
   columns.forEach((column) => {
-    const values: any[] = [];
-    for (let i = 0; i < numRows; i++) {
-      values.push(`Sample ${i + 1}`);
-    }
-    exampleData[column.field_key] = values;
+    exampleData[column.field_key] = Array(numRows).fill('Sample');
   });
 
   return exampleData;
