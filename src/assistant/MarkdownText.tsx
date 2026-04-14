05fbae5b107988e06d9b193eef4378742994eb43
varygx <69766438+varygx@users.noreply.github.com>
2026-04-14 13:35:47 -0400

    feat: Style links

--- src/assistant/MarkdownText.tsx
+++ src/assistant/MarkdownText.tsx
@@ -1,12 +1,39 @@
-import { memo } from 'react';
-import { Streamdown } from 'streamdown';
+import { memo, useMemo } from 'react';
+import { css } from '@emotion/react';
+import { Streamdown, type Components } from 'streamdown';
 
 interface MarkdownTextProps {
   text: string;
 }
 
+const assistantLink = css({
+  color: '#2563eb',
+  fontWeight: 400,
+  textDecoration: 'underline',
+  textUnderlineOffset: '0.12em',
+  textDecorationSkipInk: 'auto',
+  overflowWrap: 'break-word',
+  '&:visited': {
+    color: '#6d28d9'
+  },
+  '&:hover': {
+    color: '#1d4ed8'
+  }
+});
+
 const MarkdownText = memo(({ text }: MarkdownTextProps) => {
-  return <Streamdown>{text}</Streamdown>;
+  const components = useMemo<Components>(
+    () => ({
+      a: ({ href, children }) => (
+        <a href={href} css={assistantLink}>
+          {children}
+        </a>
+      )
+    }),
+    []
+  );
+
+  return <Streamdown components={components}>{text}</Streamdown>;
 });
 
 MarkdownText.displayName = 'MarkdownText';
