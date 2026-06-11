c791f996eae25a1dd3a352c2148e068672885805
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2026-06-11 11:18:58 -0500

    chore: rm CacheProvider from AssistantChat

--- src/assistant/index.tsx
+++ src/assistant/index.tsx
@@ -1,17 +1,14 @@
 import { lazy, Suspense } from 'react';
 import type { AssistantChatProps } from './AssistantChat';
-import { FeatheryCacheProvider } from '../utils/emotionCache';
 
 const LazyAssistantChat = lazy(
   () => import(/* webpackChunkName: "AssistantChat" */ './AssistantChat')
 );
 
 export const AssistantChat = (props: AssistantChatProps) => (
-  <FeatheryCacheProvider>
-    <Suspense fallback={null}>
-      <LazyAssistantChat {...props} />
-    </Suspense>
-  </FeatheryCacheProvider>
+  <Suspense fallback={null}>
+    <LazyAssistantChat {...props} />
+  </Suspense>
 );
 
 export type { AssistantChatProps };
