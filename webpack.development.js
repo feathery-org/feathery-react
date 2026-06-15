19aa15628da4345b058436ffa7796d41a89913b9
Tyler Trebitowski <tyler.trebitowski@gmail.com>
2026-06-15 13:32:44 -0400

    fix: Dev build

--- webpack.development.js
+++ webpack.development.js
@@ -12,9 +12,24 @@ module.exports = (env) => {
   }
 
   config.output.path = path.resolve(__dirname, 'dist');
+  config.output.publicPath = '';
+  config.module.parser = {
+    ...config.module.parser,
+    javascript: {
+      ...config.module.parser?.javascript,
+      url: 'relative'
+    }
+  };
 
   config.devtool = 'eval-cheap-module-source-map';
-  config.externals = ['react'];
+  config.externals = [
+    'react',
+    'react-dom',
+    'react-dom/client',
+    'react-dom/server',
+    'react/jsx-runtime',
+    'react/jsx-dev-runtime'
+  ];
   config.plugins.push(
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
