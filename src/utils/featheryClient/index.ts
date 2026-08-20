e56d7e841b6982ea13a09038f2804625e8f816a8
Allen Uy <69766438+varygx@users.noreply.github.com>
2026-08-19 22:35:04 -0400

    fix: Send the form's panel key with computer agent run triggers (#1777)

--- src/utils/featheryClient/index.ts
+++ src/utils/featheryClient/index.ts
@@ -1025,7 +1025,8 @@ export default class FeatheryClient extends IntegrationClient {
       method: 'POST',
       body: JSON.stringify({
         agent_id: agentId,
-        fuser_key: userId
+        fuser_key: userId,
+        panel_key: this.formKey
       })
     };
     const res = await this._fetch(url, reqOptions, false);
